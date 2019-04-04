58e138b536fb6d2c6030b18a1cfbf2df5b5d775e
Dmitry <38247163+MakeMeLucky@users.noreply.github.com>
2019-04-04 11:51:09 +0300

    refactor

--- src/main/java/web/App.java
+++ src/main/java/web/App.java
@@ -1,10 +1,5 @@
 package web;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.List;
 
 public class App 
